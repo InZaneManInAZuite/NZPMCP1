5c8764e84430441a0a1e4198c6b1d61af53eec21
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2024-12-10 03:47:25 +1300

    feat: If a user is logged in, they are redirected away from login page

--- frontend/src/pages/Login.page.jsx
+++ frontend/src/pages/Login.page.jsx
@@ -1,6 +1,22 @@
 import LoginForm from '../components/LoginForm/LoginForm'
+import UserContext from '../context/UserContext';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+    
+    // Prevent logged in users from accessing the login page
+    const { isLogged } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    // Redirect the user to the landing page if they are already logged in
+    useEffect(() => {
+        if (isLogged) {
+            navigate('/');
+        }
+    }, [navigate, isLogged]);
+
+    // Render the login page
     return (
         <LoginForm />
     );
