f067a3bb9175233407ce7df3873ab742acc10cf6
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2024-12-10 22:10:32 +1300

    fix: fixed config file import issue

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,16 +1,7 @@
-//import dotenv from 'dotenv'
 
-// Configuration if Development
 const config = {
     API: 'http://localhost:3001/api'  
 }
-
-/*
-// Configuration if Production
-if (process.env.NODE_ENV === 'production') {
-    config.API = process.env.PORT
-}
-*/
 
 
 export default config
